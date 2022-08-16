8191afb4d6bcad3c59e90898e158322f1794851a
phuonghehe <102595122+phuonghehe@users.noreply.github.com>
2022-08-16 14:24:06 +0700

    a

---
+++ js.js
@@ -0,0 +1,7 @@
+let xOverlay = document.querySelector('.overlay');
+function onBars(){
+    xOverlay.classList.toggle('overlay');
+}
+function closeX(){
+    xOverlay.classList.toggle('overlay');
+}
